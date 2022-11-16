d2022a5aa36eca0ed5ab4f285ea15ee0c6ea1be7
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2022-11-16 22:28:13 +0100

    FINALLY

--- src/main.py
+++ src/main.py
@@ -21,12 +21,12 @@ def playAll (libreriaVar):
 if __name__ == "__main__":
 
     libreriaVar = {"California Uber Alles":
-                {"track-number": 2, "location": "C:\\Users\\abelc\\Desktop\\PROYECTO-PYTHO\\Proyect-VLC-shuffle\\libreria\\California_Uber_Alles.mp3"},
+                {"track-number": 2, "location": ".\\libreria\\California_Uber_Alles.mp3"},
             "King_Kunta": 
-                {"track-number": 5, "location": "C:\\Users\\abelc\\Desktop\\PROYECTO-PYTHO\\Proyect-VLC-shuffle\\libreria\\King_Kunta.mp3"},
+                {"track-number": 5, "location": ".\\libreria\\King_Kunta.mp3"},
             "Against the moon":
-                {"track-number": 1, "location": "C:\\Users\\abelc\\Desktop\\PROYECTO-PYTHO\\Proyect-VLC-shuffle\\libreria\\against_the_moon.mp3"},
+                {"track-number": 1, "location": ".\\libreria\\against_the_moon.mp3"},
             "Headless":
-                {"track-number": 4,  "location": "C:\\Users\\abelc\\Desktop\\PROYECTO-PYTHO\\Proyect-VLC-shuffle\\libreria\\Headless.mp3"}
+                {"track-number": 4,  "location": ".\\libreria\\Headless.mp3"}
             }
     playAll(libreriaVar)
